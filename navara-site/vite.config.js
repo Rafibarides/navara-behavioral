feaa1e2166f1c855285c8c43f11cbf4801db0bdb
Rafibarides <rafibaridesmusic@gmail.com>
2025-06-13 15:14:43 -0400

    new domain

--- navara-site/vite.config.js
+++ navara-site/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  // Use / for custom domain (navarabg.com) in both production and development
+  // Use / for custom domain (navarabehavioralgroup.com) in both production and development
   const base = '/';
   
   return {
